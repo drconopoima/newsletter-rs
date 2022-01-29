97e8863c19254af67d15753d21d76da59c4ce916
Luis Jesus Diaz Manzo <luis@drconopoima.com>
2022-01-29 22:39:57 +0100

    scripts/launch_postgres.bash minor improvement

--- scripts/launch_postgres.bash
+++ scripts/launch_postgres.bash
@@ -137,7 +137,7 @@ find . -type f -name "*.sql" -print0 | sort -z | while IFS= read -r -d '' script
     sqlfilename=$(basename ${script});
     printf "Running migration script %s...\n" "${script}"
     if grep '^COMMIT;$' "${script}" 1>/dev/null 2>&1; then
-        sed 's/COMMIT/ROLLBACK/g' "${script}" | psql --quiet "postgresql://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}" && \
+        sed 's/^COMMIT;/ROLLBACK;/g' "${script}" | psql --quiet "postgresql://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}" && \
         printf "[PASS] Tested script '%s' successfully\n" "${sqlfilename}" && \
         psql "postgresql://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}" --file="${script}" && \
         psql "postgresql://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}" -c "INSERT into _initialization_migrations ( filename, md5_hash ) VALUES ( '${sqlfilename}', '${md5}' )" && \
