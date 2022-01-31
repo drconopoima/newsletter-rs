3adf97494e72faa19804093e862525e6d9dd5760
Luis Jesus Diaz Manzo <luis@drconopoima.com>
2022-01-31 20:50:25 +0100

    tests/healthcheck.rs connect and query Postgres

--- tests/healthcheck.rs
+++ tests/healthcheck.rs
@@ -13,6 +13,12 @@ fn launch_http_server() -> String {
     format!("http://{}:{}", local_addr, port)
 }
 
+#[derive(serde::Serialize)]
+struct Body {
+    email: String,
+    name: String,
+}
+
 #[actix_rt::test]
 async fn healthcheck_endpoint() {
     // Arrange
@@ -43,26 +49,40 @@ async fn subscription_200_valid_form_data() {
         &config_file
     ));
     let pg_connection_string: String = configuration.database.connection_string();
-    let _pg_connection: PgConnection =
-        PgConnection::connect(&pg_connection_string)
-            .await
-            .expect(&format!(
-                "ERROR: Failed to connect to Postgres at URL: {}",
-                &pg_connection_string
-            ));
+    let mut pg_connection: PgConnection = PgConnection::connect(&pg_connection_string)
+        .await
+        .expect(&format!(
+            "ERROR: Failed to connect to Postgres at URL: {}",
+            &pg_connection_string
+        ));
     let client = reqwest::Client::new();
-    let body = "email=email_nobody_has%40drconopoima.com&name=Jane%20Doe";
+    let email_field = "email_nobody_has@drconopoima.com";
+    let name_field = "Jane Doe";
+    let body = Body {
+        email: email_field.to_string(),
+        name: name_field.to_string(),
+    };
+    let body_encoded = serde_urlencoded::to_string(&body).unwrap();
     let subscriptions_route = &format!("{}/subscription", server_address);
     // Act
     let response = client
         .post(subscriptions_route)
         .header("Content-Type", "application/x-www-form-urlencoded")
-        .body(body)
+        .body(body_encoded)
         .send()
         .await
         .expect(&format!("Failed POST request to {}", subscriptions_route));
     // Assert
     assert_eq!(200, response.status().as_u16());
+    let row_results: (String, String) = sqlx::query_as(
+        "SELECT email::text as email,name FROM newsletter.subscription WHERE email=$1",
+    )
+    .bind(body.email)
+    .fetch_one(&mut pg_connection)
+    .await
+    .expect("Failed to fetch saved subscription.");
+    assert_eq!(row_results.0, email_field);
+    assert_eq!(row_results.1, name_field);
 }
 
 #[actix_rt::test]
