e86770838d0c77a9a419b25cbc8df4f0b617aaa4
Luis Jesus Diaz Manzo <luis@drconopoima.com>
2022-01-31 11:30:33 +0100

    scripts/launch_postgres.bash bugfix help message

--- scripts/launch_postgres.bash
+++ scripts/launch_postgres.bash
@@ -2,47 +2,49 @@
 # set -x
 set -Eeuo pipefail
 #
-## Section Help
-function help(){
-printf "%s [-hq] (v%s)\n" "${SCRIPT_NAME}" "${SCRIPT_VERSION}"
-printf "Launch a containerized PostgreSQL database for newsletter-rs\n"
-printf "\n"
-printf "\t -h: Show this help message\n"
-printf "Container Engine: Defaults to Podman (if available in PATH). Otherwise Docker (if available in PATH)\n"
-printf "\n"
-printf "Parameters are customized by exporting any of the following environment variables:\n"
-printf "\t POSTGRES_USER: User for postgres. Default: 'postgres'\n"
-printf "\t POSTGRES_PASSWORD: Password for POSTGRES_USER. Default: 'password'\n"
-printf "\t POSTGRES_DB: User for postgres. Default: 'newsletter'\n"
-printf "\t POSTGRES_PORT: Bind Port for postgres. Default: '5432'\n"
-printf "\t POSTGRES_VERSION: Container version at registry. Default: 'latest'\n"
-printf "\t CONTAINER_REGISTRY: Container registry to pull from. Default: 'docker.io/library/postgres'\n"
-}
-readonly -f help
-
-while getopts ':h' option; do
-case "$option" in
-    h)  # display help
-        help
-        exit 0
-    ;;
-    * )
-        echo "Error: Invalid option $*"
-        help
-        exit 2
-esac
-done
-
 ## Section Script Identification
 readonly SCRIPT_CALLNAME="${0}"
 SCRIPT_NAME="$(basename -- "${SCRIPT_CALLNAME}" 2>/dev/null)"
 readonly SCRIPT_NAME
 readonly SCRIPT_VERSION="0.3.0"
+
+## Section Help
+function help {
+    printf "%s [-h] (v%s)\n" "${SCRIPT_NAME}" "${SCRIPT_VERSION}"
+    printf "Launch a containerized PostgreSQL database for newsletter-rs\n"
+    printf "\t -h: Show this help message\n"
+    printf "\n"
+    printf "Container Engine: Defaults to Podman (if available in PATH). Otherwise Docker (if available)\n"
+    printf "\n"
+    printf "Parameters are customized by exporting any of the following environment variables:\n"
+    printf "\t POSTGRES_USER: User for postgres. Default: 'postgres'\n"
+    printf "\t POSTGRES_PASSWORD: Password for POSTGRES_USER. Default: 'password'\n"
+    printf "\t POSTGRES_DB: User for postgres. Default: 'newsletter'\n"
+    printf "\t POSTGRES_PORT: Bind Port for postgres. Default: '5432'\n"
+    printf "\t POSTGRES_VERSION: Container version at registry. Default: 'latest'\n"
+    printf "\t CONTAINER_REGISTRY: Container registry to pull from. Default: 'docker.io/library/postgres'\n"
+}
+readonly -f help
+
+while getopts ':h' option; do
+    case "$option" in
+        h)  # display help
+            help
+            exit 0
+        ;;
+        * )
+            echo "Error: Invalid option $*"
+            help
+            exit 2
+    esac
+done
+
+# Section Parent Path
 NEWSLETTER_RS_PATH="$( cd -- "$(dirname "${SCRIPT_CALLNAME}")/../" >/dev/null 2>&1 ; pwd -P )"
 readonly NEWSLETTER_RS_PATH
 NEWSLETTER_RS_VERSION="$(grep -m1 '^version' "${NEWSLETTER_RS_PATH}/Cargo.toml" | awk -F "\"" '{ print $2 }' )"
 readonly NEWSLETTER_RS_VERSION
-printf "%s (v%s, newsletter-rs v%s)\n" "${SCRIPT_NAME}" "${SCRIPT_VERSION}" "${NEWSLETTER_RS_VERSION}"
+printf "%s (v%s, newsletter-rs v%s)\n" "${SCRIPT_NAME}" "${SCRIPT_VERSION}" "${NEWSLETTER_RS_VERSION}"\
 
 ## Section validate dependencies
 if ! command -v psql 1>/dev/null 2>&1; then
@@ -53,7 +55,7 @@ if command -v podman 1>/dev/null 2>&1; then
     function containertech {
         podman $@
     }
-elif command -v docker 1>/dev/null 2>&1; then
+    elif command -v docker 1>/dev/null 2>&1; then
     function containertech {
         docker $@
     }
@@ -85,13 +87,13 @@ if [ ! "$(containertech ps -aq -f name="^${CONTAINER_NAME}$")" ]; then
     printf "When ready, clean-up by running:\n"
     printf "\t {podman/docker} stop %s\n" "${CONTAINER_NAME}"
     containertech run -d --rm --name ${CONTAINER_NAME} \
-        -e POSTGRES_USER=${DB_USER} \
-        -e POSTGRES_PASSWORD=${DB_PASSWORD} \
-        -e POSTGRES_DB=${DB_NAME} \
-        -p "${DB_PORT}":5432 \
-        ${DB_REGISTRY}:${DB_VERSION} \
-        postgres -N 1000 1>/dev/null
-        # ^ Increased maximum number of connections for testing purposes
+    -e POSTGRES_USER=${DB_USER} \
+    -e POSTGRES_PASSWORD=${DB_PASSWORD} \
+    -e POSTGRES_DB=${DB_NAME} \
+    -p "${DB_PORT}":5432 \
+    ${DB_REGISTRY}:${DB_VERSION} \
+    postgres -N 1000 1>/dev/null
+    # ^ Increased maximum number of connections for testing purposes
 else
     printf "ERROR: There exists a container called '%s'\n" "${CONTAINER_NAME}"
     printf "\n"
@@ -108,9 +110,9 @@ fi
 # Ping until Postgres startup is validated.
 wait_time=1
 until psql postgresql://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME} -c '\q' 2>/dev/null; do
-  >&2 echo "[WARN] Postgres is still unavailable - waiting ${wait_time} second(s)..."
-  sleep "${wait_time}"
-  wait_time=$(( wait_time * 2 ))
+    >&2 echo "[WARN] Postgres is still unavailable - waiting ${wait_time} second(s)..."
+    sleep "${wait_time}"
+    wait_time=$(( wait_time * 2 ))
 done
 
 printf "[PASS] Postgres is running and ready\n"
@@ -119,7 +121,7 @@ cd "${NEWSLETTER_RS_PATH}/migrations" || exit;
 find . -type f -name "*.sql" -print0 | sort -z | while IFS= read -r -d '' script; do
     if command -v md5sum 1>/dev/null 2>&1; then
         md5="$(md5sum "${script}" | awk '{ print $1 }')";
-    elif command -v md5 1>/dev/null 2>&1; then
+        elif command -v md5 1>/dev/null 2>&1; then
         md5="$(md5 "${script}")";
     fi
     sqlfilename=$(basename "${script}");
