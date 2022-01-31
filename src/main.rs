73bd9fab79a1b4892cd232fac9da2d2b542cb087
Luis Jesus Diaz Manzo <luis@drconopoima.com>
2022-01-31 21:44:58 +0100

    main.rs clippy warnings expect followed by function call

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,11 @@ use std::net::TcpListener;
 async fn main() -> std::io::Result<()> {
     //
     let config_file: &str = "configuration";
-    let configuration: Settings = get_configuration(&config_file).expect(&format!(
-        "ERROR: Failed to read configuration file: '{}'",
-        &config_file
-    ));
+    let configuration: Settings = get_configuration(config_file).unwrap_or_else(
+        |error| {
+            panic!("ERROR: Failed to read configuration file \"{}\", {}.",
+                config_file, error)
+        });
     let bind_address: (&str, u16) = ("127.0.0.1", configuration.application_port);
     // Raises if failed to bind address
     let listener = TcpListener::bind(bind_address)?;
