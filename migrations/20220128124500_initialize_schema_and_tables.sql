b398e4a5da3c8c4247ac07adcd66d6ccdd67b084
Luis Jesus Diaz Manzo <luis@drconopoima.com>
2022-02-02 20:52:40 +0100

    Remove side-effect table from SQLx removed dependency

--- migrations/20220128124500_initialize_schema_and_tables.sql
+++ migrations/20220128124500_initialize_schema_and_tables.sql
@@ -10,14 +10,4 @@ CREATE TABLE IF NOT EXISTS _initialization_migrations(
     md5_hash UUID NOT NULL
 );
 
--- For eventual SQLx-handled migrations
-CREATE TABLE IF NOT EXISTS _sqlx_migrations (
-    version BIGINT PRIMARY KEY,
-    description TEXT NOT NULL,
-    installed_on TIMESTAMPTZ NOT NULL DEFAULT now(),
-    success BOOLEAN NOT NULL,
-    checksum BYTEA NOT NULL,
-    execution_time BIGINT NOT NULL
-);
-
 COMMIT;
