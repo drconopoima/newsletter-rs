0ea720a6c088d52ffc59de64a9c866c781f17665
Luis Jesus Diaz Manzo <luis@drconopoima.com>
2022-02-12 13:21:18 +0100

    testdata/k6_post_subscription.js diminish duplicate emails

--- testdata/k6_post_subscription.js
+++ testdata/k6_post_subscription.js
@@ -5,7 +5,7 @@ export default function () {
       'Content-Type': 'application/x-www-form-urlencoded',
     },
   };
-  let randomnumber = String(Math.floor(Math.random() * (10000000 - 1)));
-  let data = `email=${randomnumber}%40gmail.com&name=${randomnumber}`;
+  let randomnumber = String(Math.floor(Math.random() * (10000000000 - 1)));
+  let data = `email=${randomnumber}%40drconopoima.com&name=${randomnumber}`;
   http.post('http://localhost:8000/subscription', data,params);
 }
