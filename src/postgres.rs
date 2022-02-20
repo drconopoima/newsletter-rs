22c6c6d11db9f9204ae4957884403929c256a24e
Luis Jesus Diaz Manzo <luis@drconopoima.com>
2022-02-20 23:08:19 +0100

    postgres.rs ensure migrations tracking

--- src/postgres.rs
+++ src/postgres.rs
@@ -1,9 +1,11 @@
 use crate::configuration::DatabaseSettings;
 use deadpool_postgres::{Manager, ManagerConfig, Object, Pool, RecyclingMethod};
+use md5;
 use std::fs::{read_dir, File};
 use std::io::{BufReader, Read};
 use std::str::FromStr;
-use tokio_postgres::{NoTls, SimpleQueryMessage};
+use tokio_postgres::{Error, NoTls, SimpleQueryMessage};
+use uuid::Uuid;
 
 pub fn generate_connection_pool(postgres_connection_string: String) -> Pool {
     let postgres_configuration =
@@ -67,8 +69,7 @@ pub async fn create_database(database_settings: &mut DatabaseSettings) -> Pool {
         database_settings.database = Some(database_name.to_string());
     } else {
         let create_database_query = format!("CREATE DATABASE \"{}\"", database_name);
-        postgres_client
-            .simple_query(&create_database_query)
+        run_simple_query(&postgres_client, &create_database_query)
             .await
             .expect("Failed to create database");
     }
@@ -95,6 +96,7 @@ pub async fn migrate_database(
     for path in path_entries {
         migration_script_paths.push(path.path().display().to_string());
     }
+    create_migrations_table(&postgres_client).await;
     for migration_script_path in migration_script_paths {
         let migration_file = File::open(&migration_script_path).unwrap();
         let mut reader = BufReader::new(migration_file);
@@ -106,8 +108,7 @@ pub async fn migrate_database(
             )
         });
         let migration_file_contents = String::from_utf8_lossy(&buffer).to_string();
-        postgres_client
-            .simple_query(&migration_file_contents)
+        run_simple_query(&postgres_client, &migration_file_contents)
             .await
             .unwrap_or_else(|error| {
                 panic!(
@@ -115,6 +116,57 @@ pub async fn migrate_database(
                     &migration_script_path, error
                 )
             });
+        let md5_script_digest = md5::compute(&migration_file_contents);
+        let md5_script_string = format!("{:x}", md5_script_digest);
+        let script_md5_uuid = Uuid::parse_str(&md5_script_string).unwrap();
+        let file_path = std::path::Path::new(&migration_script_path);
+        let filename_script: &str = file_path.file_name().unwrap().to_str().unwrap();
+        let statement = match postgres_client
+            .prepare_cached(
+                r#"
+            INSERT into _initialization_migrations (filename, md5_hash)
+                VALUES ($1, $2)
+            "#,
+            )
+            .await
+        {
+            Ok(statement) => Some(statement),
+            Err(error) => {
+                panic!("Failed to prepare cached insert migration: {}", error);
+            }
+        };
+        postgres_client
+            .query(&statement.unwrap(), &[&filename_script, &script_md5_uuid])
+            .await
+            .unwrap_or_else(|error| panic!("Failed to insert migration: {}", error));
     }
     postgres_pool
+}
+
+pub async fn create_migrations_table(postgres_client: &Object) {
+    let migration_table_name = "_initialization_migrations";
+    let create_table_statement = format!(
+        "CREATE TABLE IF NOT EXISTS {}(
+        version SERIAL PRIMARY KEY,
+        filename TEXT NOT NULL,
+        installed_on TIMESTAMPTZ NOT NULL DEFAULT now(),
+        md5_hash UUID NOT NULL
+    );",
+        &migration_table_name
+    );
+    let _ = run_simple_query(postgres_client, &create_table_statement)
+        .await
+        .unwrap_or_else(|error| {
+            panic!(
+                "Failed to create table \"{}\" with query \"{}\": {}",
+                migration_table_name, create_table_statement, error
+            )
+        })[0];
+}
+
+pub async fn run_simple_query(
+    postgres_client: &Object,
+    query_statement: &str,
+) -> Result<Vec<SimpleQueryMessage>, Error> {
+    postgres_client.simple_query(query_statement).await
 }
