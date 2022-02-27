c500d51c77d30fba32c47640d481f603c0d6ecbd
Luis Jesus Diaz Manzo <luis@drconopoima.com>
2022-02-27 18:05:57 +0100

    launch_postgres v0.8.0 use pg_isready and access host env var

--- scripts/launch_postgres.bash
+++ scripts/launch_postgres.bash
@@ -10,7 +10,7 @@ set -Eeuo pipefail
 readonly SCRIPT_CALLNAME="${0}"
 SCRIPT_NAME="$(basename -- "${SCRIPT_CALLNAME}" 2>/dev/null)"
 readonly SCRIPT_NAME
-readonly SCRIPT_VERSION="0.7.0"
+readonly SCRIPT_VERSION="0.8.0"
 
 ## Section Help
 function print_help {
@@ -24,6 +24,7 @@ function print_help {
     printf "    POSTGRES_USER: User for postgres. Default: 'postgres'\n"
     printf "    POSTGRES_PASSWORD: Password for POSTGRES_USER. Default: 'password'\n"
     printf "    POSTGRES_DB: User for postgres. Default: 'newsletter'\n"
+    printf "    POSTGRES_HOST: Host endpoint for postgres. Default: 'localhost'\n"
     printf "    POSTGRES_PORT: Bind Port for postgres. Default: '5432'\n"
     printf "    POSTGRES_VERSION: Container version at registry. Default: 'latest'\n"
     printf "    CONTAINER_REGISTRY: Container registry to pull from. Default: 'docker.io/library/postgres'\n"
@@ -89,6 +90,8 @@ readonly DB_PORT=${POSTGRES_PORT:="5432"}
 readonly DB_VERSION=${POSTGRES_VERSION:="latest"}
 # Check if a custom container registry has been set, otherwise default to 'docker.io/library/postgres'
 readonly DB_REGISTRY=${CONTAINER_REGISTRY:="docker.io/library/postgres"}
+# Check if a custom postgres host endpoint has been set, otherwise default to 'localhost'
+readonly DB_HOST=${POSTGRES_HOST:='localhost'}
 readonly CONTAINER_NAME="newsletter-rs-db"
 
 if [[ -z ${SKIP_CONTAINER+undeclared} ]]; then
@@ -131,19 +134,19 @@ fi
 # Create temporary password file
 readonly PGPASSFILE='/tmp/launch_postgres_pgpass'
 export PGPASSFILE
-echo -n "localhost:${DB_PORT}:*:${DB_USER}:${POSTGRES_PASSWORD}" | tee "${PGPASSFILE}" >/dev/null
+echo -n "${DB_HOST}:${DB_PORT}:*:${DB_USER}:${POSTGRES_PASSWORD}" | tee "${PGPASSFILE}" >/dev/null
 chmod 600 "${PGPASSFILE}"
 trap 'rm '"${PGPASSFILE}"'' EXIT
 # Ping until Postgres startup is validated.
 wait_time=1
-until psql -h localhost -p "${DB_PORT}" -U "${DB_USER}" -v PGPASSFILE="${PGPASSFILE}" -c '\q'; do
+until pg_isready -h "${DB_HOST}" -p "${DB_PORT}" -U "${DB_USER}"; do
     >&2 echo "[WARN] Postgres is still unavailable - waiting ${wait_time} second(s)..."
     sleep "${wait_time}"
     wait_time=$(( wait_time * 2 ))
 done
 printf "[PASS] Postgres is running and ready\n"
 printf "Creating Database Newsletter if not available\n"
-echo "SELECT 'CREATE DATABASE ${DB_NAME}' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '${DB_NAME}')\gexec" |  psql -v ON_ERROR_STOP=1 -h localhost -p "${DB_PORT}" -U "${DB_USER}" -v PGPASSFILE="${PGPASSFILE}"
+echo "SELECT 'CREATE DATABASE ${DB_NAME}' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '${DB_NAME}')\gexec" |  psql -v ON_ERROR_STOP=1 -h "${DB_HOST}" -p "${DB_PORT}" -U "${DB_USER}" -v PGPASSFILE="${PGPASSFILE}" -w
 printf "[PASS] Database '%s' ready to use\n" "${DB_NAME}"
 
 cd "${NEWSLETTER_RS_PATH}/migrations" || exit;
@@ -154,7 +157,7 @@ find . -type f -name "*.sql" -print0 | sort -z | while IFS= read -r -d '' script
         md5="$(md5 "${script}")";
     fi
     sqlfilename=$(basename "${script}");
-    if [[ "$( psql -t -h localhost -p \"${DB_PORT}\" -U \"${DB_USER}\" -d \"${DB_NAME}\" -v PGPASSFILE="${PGPASSFILE}" -c "SELECT 1 FROM _initialization_migrations WHERE filename='${sqlfilename}' LIMIT 1" 2>/dev/null | awk '{ print $1 }')" -eq 1 ]]; then
+    if [[ "$( psql -t -h $\"${DB_HOST}\" -p \"${DB_PORT}\" -U \"${DB_USER}\" -d \"${DB_NAME}\" -v PGPASSFILE="${PGPASSFILE}" -w -c "SELECT 1 FROM _initialization_migrations WHERE filename='${sqlfilename}' LIMIT 1" 2>/dev/null | awk '{ print $1 }')" -eq 1 ]]; then
         printf "[WARN] Skipping script '%s' as it's already applied\n" "${sqlfilename}";
         continue
     fi
@@ -162,7 +165,7 @@ find . -type f -name "*.sql" -print0 | sort -z | while IFS= read -r -d '' script
     if grep '^COMMIT;$' "${script}" 1>/dev/null 2>&1; then
         shopt -s lastpipe
         set +Ee
-        sed 's/^COMMIT;/ROLLBACK;/g' "${script}" |  psql -v ON_ERROR_STOP=1 --quiet -h localhost -p "${DB_PORT}" -U "${DB_USER}" -d "${DB_NAME}" -v PGPASSFILE="${PGPASSFILE}" 2>&1 | rollbackoutput=$(</dev/stdin)
+        sed 's/^COMMIT;/ROLLBACK;/g' "${script}" |  psql -v ON_ERROR_STOP=1 --quiet -h "${DB_HOST}" -p "${DB_PORT}" -U "${DB_USER}" -d "${DB_NAME}" -v PGPASSFILE="${PGPASSFILE}" -w 2>&1 | rollbackoutput=$(</dev/stdin)
         returncode="$?"
         shopt -u lastpipe
         set -Ee
@@ -173,14 +176,14 @@ find . -type f -name "*.sql" -print0 | sort -z | while IFS= read -r -d '' script
             echo "${rollbackoutput}"
             exit 4
         fi
-        psql -h localhost -p "${DB_PORT}" -U "${DB_USER}" -d "${DB_NAME}" -v PGPASSFILE="${PGPASSFILE}" --file="${script}" && \
-        psql -h localhost -p "${DB_PORT}" -U "${DB_USER}" -d "${DB_NAME}" -v PGPASSFILE="${PGPASSFILE}" -c "INSERT into _initialization_migrations ( filename, md5_hash ) VALUES ( '${sqlfilename}', '${md5}' )" && \
+        psql -h "${DB_HOST}" -p "${DB_PORT}" -U "${DB_USER}" -d "${DB_NAME}" -v PGPASSFILE="${PGPASSFILE}" -w --file="${script}" && \
+        psql -h "${DB_HOST}" -p "${DB_PORT}" -U "${DB_USER}" -d "${DB_NAME}" -v PGPASSFILE="${PGPASSFILE}" -w -c "INSERT into _initialization_migrations ( filename, md5_hash ) VALUES ( '${sqlfilename}', '${md5}' )" && \
         printf "[PASS] Applied DB migration script '%s' successfully\n" "${sqlfilename}"
     else
         printf "[WARN]: No transactions present at script '%s', applying without prior testing\n" "${sqlfilename}"
         shopt -s lastpipe
         set +Ee
-        psql -v ON_ERROR_STOP=1 -h localhost -p "${DB_PORT}" -U "${DB_USER}" -d "${DB_NAME}" -v PGPASSFILE="${PGPASSFILE}" --file="${script}"  2>&1 | rollbackoutput=$(</dev/stdin)
+        psql -v ON_ERROR_STOP=1 -h "${DB_HOST}" -p "${DB_PORT}" -U "${DB_USER}" -d "${DB_NAME}" -v PGPASSFILE="${PGPASSFILE}" -w --file="${script}"  2>&1 | rollbackoutput=$(</dev/stdin)
         returncode="$?"
         shopt -u lastpipe
         set -Ee
