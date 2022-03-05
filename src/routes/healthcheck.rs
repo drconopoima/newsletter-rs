90328798888005e8d30133b5a531e6fd259935db
Luis Jesus Diaz Manzo <luis@drconopoima.com>
2022-03-05 23:42:01 +0100

    healthcheck fail response on pool/client errors

--- src/routes/healthcheck.rs
+++ src/routes/healthcheck.rs
@@ -6,15 +6,15 @@ use std::time::SystemTime;
 
 // Healthcheck response format for HTTP APIs https://inadarei.github.io/rfc-healthcheck/
 #[derive(serde::Serialize)]
-pub struct Healthcheck {
+pub struct HealthcheckObject {
     pub status: String,
     pub checks: ChecksObject,
     pub output: String,
+    pub time: String,
 }
 
 #[derive(serde::Serialize)]
 pub struct ChecksObject {
-    pub newsletter: NewsletterChecks,
     pub postgres_read: PostgresReadChecks,
     pub postgres_write: PostgresWriteChecks,
 }
@@ -23,7 +23,7 @@ pub struct ChecksObject {
 
 pub struct PostgresReadChecks {
     pub status: String,
-    pub time: String,
+    pub time: Option<String>,
     pub output: String,
 }
 
@@ -31,20 +31,67 @@ pub struct PostgresReadChecks {
 
 pub struct PostgresWriteChecks {
     pub status: String,
-    pub time: String,
-    pub pg_isinrecovery: String,
+    pub time: Option<String>,
+    pub pg_isinrecovery: Option<String>,
     pub output: String,
 }
 
-#[derive(serde::Serialize)]
+fn postgres_read_checks(status: &str, time: Option<String>, output: &str) -> PostgresReadChecks {
+    return PostgresReadChecks {
+        status: status.to_owned(),
+        time,
+        output: output.to_owned(),
+    };
+}
 
-pub struct NewsletterChecks {
-    pub status: String,
-    pub time: String,
-    pub output: String,
+fn postgres_write_checks(
+    status: &str,
+    time: Option<String>,
+    pg_isinrecovery: Option<String>,
+    output: &str,
+) -> PostgresWriteChecks {
+    return PostgresWriteChecks {
+        status: status.to_owned(),
+        time,
+        pg_isinrecovery,
+        output: output.to_owned(),
+    };
+}
+
+fn get_healthcheck_object(
+    status: &str,
+    time: &str,
+    output: &str,
+    postgres_read: PostgresReadChecks,
+    postgres_write: PostgresWriteChecks,
+) -> HealthcheckObject {
+    let checks = ChecksObject {
+        postgres_read,
+        postgres_write,
+    };
+
+    HealthcheckObject {
+        status: status.to_owned(),
+        checks,
+        time: time.to_owned(),
+        output: output.to_owned(),
+    }
 }
 
 pub async fn healthcheck(request: HttpRequest) -> impl Responder {
+    let now_systemtime = SystemTime::now();
+    let now_datetime: DateTime<Utc> = now_systemtime.into();
+    let now_string = now_datetime.to_rfc3339();
+    let status_pass = "pass";
+    let status_fail = "fail";
+    let status_warn = "warn";
+    let output_pass = "";
+    let global_status: &str;
+    let postgres_read: PostgresReadChecks;
+    let postgres_write: PostgresWriteChecks;
+    let postgres_read_status: &str;
+    let postgres_write_status: &str;
+    let postgres_pool_error = "DB pool error.";
     let optional_postgres_pool: Option<&Arc<Pool>> = match request.app_data::<Arc<Pool>>() {
         Some(postgres_pool) => Some(postgres_pool),
         None => {
@@ -53,10 +100,22 @@ pub async fn healthcheck(request: HttpRequest) -> impl Responder {
         }
     };
     if optional_postgres_pool.is_none() {
-        return HttpResponse::InternalServerError()
-            .body("DB pool error while processing subscription.");
+        postgres_read_status = status_fail;
+        postgres_write_status = status_fail;
+        global_status = status_warn;
+        postgres_read = postgres_read_checks(postgres_read_status, None, postgres_pool_error);
+        postgres_write =
+            postgres_write_checks(postgres_write_status, None, None, postgres_pool_error);
+        return HttpResponse::Ok().json(get_healthcheck_object(
+            global_status,
+            &now_string,
+            output_pass,
+            postgres_read,
+            postgres_write,
+        ));
     }
     let postgres_pool = optional_postgres_pool.unwrap();
+    let postgres_client_error = "DB client error";
     let optional_postgres_client = match postgres_pool.get().await {
         Ok(manager) => Some(manager),
         Err(error) => {
@@ -65,44 +124,32 @@ pub async fn healthcheck(request: HttpRequest) -> impl Responder {
         }
     };
     if optional_postgres_client.is_none() {
-        return HttpResponse::InternalServerError()
-            .body("DB client error while processing subscription.");
+        postgres_read_status = status_fail;
+        postgres_write_status = status_fail;
+        global_status = status_warn;
+        postgres_read = postgres_read_checks(postgres_read_status, None, postgres_client_error);
+        postgres_write =
+            postgres_write_checks(postgres_write_status, None, None, postgres_client_error);
+        return HttpResponse::Ok().json(get_healthcheck_object(
+            global_status,
+            &now_string,
+            output_pass,
+            postgres_read,
+            postgres_write,
+        ));
     }
     let _postgres_client = optional_postgres_client.unwrap();
+    postgres_read_status = status_pass;
+    postgres_write_status = status_pass;
+    global_status = status_pass;
+    postgres_read = postgres_read_checks(postgres_read_status, None, "");
+    postgres_write = postgres_write_checks(postgres_write_status, None, None, "");
 
-    let now_systemtime = SystemTime::now();
-    let now_datetime: DateTime<Utc> = now_systemtime.into();
-    let now_string = now_datetime.to_rfc3339();
-    let status = "pass";
-    let output = "";
-    let postgres_read = PostgresReadChecks {
-        status: status.to_owned(),
-        time: now_string.to_owned(),
-        output: output.to_owned(),
-    };
-    let postgres_write = PostgresWriteChecks {
-        status: status.to_owned(),
-        time: now_string.to_owned(),
-        pg_isinrecovery: "false".to_owned(),
-        output: output.to_owned(),
-    };
-    let newsletter = NewsletterChecks {
-        status: status.to_owned(),
-        time: now_string,
-        output: output.to_owned(),
-    };
-
-    let checks = ChecksObject {
-        newsletter,
+    HttpResponse::Ok().json(get_healthcheck_object(
+        global_status,
+        &now_string,
+        output_pass,
         postgres_read,
         postgres_write,
-    };
-
-    let healthcheck = Healthcheck {
-        status: status.to_owned(),
-        checks,
-        output: output.to_owned(),
-    };
-
-    HttpResponse::Ok().json(healthcheck)
+    ))
 }
