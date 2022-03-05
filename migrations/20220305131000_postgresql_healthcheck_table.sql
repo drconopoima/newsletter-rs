ad29464690fc31d30e06956fdacee228f5ffbb09
Luis Jesus Diaz Manzo <luis@drconopoima.com>
2022-03-05 21:19:53 +0100

    migrations/*postgresql_healthcheck_table.sql updated_by

--- migrations/20220305131000_postgresql_healthcheck_table.sql
+++ migrations/20220305131000_postgresql_healthcheck_table.sql
@@ -1,26 +1,56 @@
 BEGIN;
 
 CREATE UNLOGGED TABLE IF NOT EXISTS _healthcheck (
-   id bool PRIMARY KEY DEFAULT TRUE,
-   datetime timestamptz DEFAULT NOW(),
-   CONSTRAINT _healthcheck_unique_row CHECK (id)
+    id bool UNIQUE NOT NULL DEFAULT TRUE,
+    datetime timestamptz NOT NULL DEFAULT NOW(),
+    updated_by char varying(126) NOT NULL,
+    CONSTRAINT _healthcheck_unique_row CHECK (id)
 );
 
+REVOKE DELETE, TRUNCATE ON public._healthcheck FROM public;
 
-CREATE OR REPLACE FUNCTION _healthcheck_ensure_timestamp_nonfuture() RETURNS TRIGGER AS $_healthcheck_timestamp_nonfuture$
+INSERT INTO _healthcheck (id, updated_by)
+VALUES (true, '20220305131000_postgresql_healthcheck_table.sql')
+ON CONFLICT ON CONSTRAINT _healthcheck_id_key 
+DO 
+    UPDATE SET updated_by = '20220305131000_postgresql_healthcheck_table.sql';
+
+-- Create user 'newsletter'
+DO
+$do$
+BEGIN
+    IF NOT EXISTS (
+        SELECT FROM pg_catalog.pg_roles
+        WHERE  rolname = 'newsletter') THEN
+        CREATE USER newsletter with ENCRYPTED PASSWORD 'password';
+    END IF;
+END
+$do$;
+
+ALTER TABLE newsletter.subscription
+OWNER TO newsletter;
+
+CREATE OR REPLACE FUNCTION trigger_set_timestamp()
+RETURNS TRIGGER AS $_healthcheck_datetime$
     BEGIN
-        --
-        -- Raise error if a future datetime tries to be inserted
-        --
-        IF( NEW.datetime > sysdate ) THEN
-            RAISE EXCEPTION 'Update of datetime is not allowed for dates in the future: %',
-                NEW.datetime;
-        END IF;
+        NEW.datetime = NOW();
+        RETURN NEW;
     END;
-$_healthcheck_timestamp_nonfuture$ LANGUAGE plpgsql;
+$_healthcheck_datetime$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE TRIGGER _healthcheck_timestamp_nonfuture
+CREATE OR REPLACE TRIGGER set_timestamp
 BEFORE INSERT OR UPDATE ON _healthcheck
-    FOR EACH ROW EXECUTE PROCEDURE _healthcheck_ensure_timestamp_nonfuture();
+    FOR EACH ROW EXECUTE PROCEDURE trigger_set_timestamp();
+REVOKE INSERT, UPDATE
+ON TABLE "_healthcheck"
+FROM public
+cascade;
+
+GRANT
+    INSERT (id, updated_by),
+    UPDATE (id, updated_by),
+    SELECT (id, datetime, updated_by)
+ON TABLE "_healthcheck"
+TO public;
 
 COMMIT;
