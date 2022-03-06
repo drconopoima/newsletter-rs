8ada04e065759a91ddc9b06cd50fe3f677c2ef25
Luis Jesus Diaz Manzo <luis@drconopoima.com>
2022-03-06 02:27:37 +0100

    healthcheck.rs cargo fmt

--- src/routes/healthcheck.rs
+++ src/routes/healthcheck.rs
@@ -18,7 +18,7 @@ pub struct HealthcheckObject {
     pub checks: ChecksObject,
     pub output: String,
     pub time: String,
-    pub version: String
+    pub version: String,
 }
 
 #[derive(serde::Serialize)]
@@ -83,7 +83,7 @@ fn get_healthcheck_object(
         checks,
         time: time.to_owned(),
         output: output.to_owned(),
-        version: env!("CARGO_PKG_VERSION").to_owned()
+        version: env!("CARGO_PKG_VERSION").to_owned(),
     }
 }
 
