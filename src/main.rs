344eeb1d65eca22a16de1c7335f88710062df735
Luis Jesus Diaz Manzo <luis@drconopoima.com>
2022-03-08 23:17:27 +0100

    main.rs Refactor tracing into functions

--- src/main.rs
+++ src/main.rs
@@ -9,21 +9,34 @@ use newsletter_rs::{
 };
 use std::net::TcpListener;
 use std::time::Duration;
-use tracing::subscriber::set_global_default;
+use tracing::{Subscriber,subscriber::set_global_default};
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_log::LogTracer;
 use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
-#[actix_web::main]
-async fn main() -> Result<(), ExitFailure> {
-    LogTracer::init()?;
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new("newsletter-rs".into(), std::io::stdout);
-    let subscriber = Registry::default()
+/// Compose multiple layers into a `tracing`'s subscriber.
+pub fn get_subscriber(
+    name: String, 
+    env_filter: String
+) -> impl Subscriber + Send + Sync {
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(env_filter));
+    let formatting_layer = BunyanFormattingLayer::new(name, std::io::stdout);
+    Registry::default()
         .with(env_filter)
         .with(JsonStorageLayer)
-        .with(formatting_layer);
-    set_global_default(subscriber)?;
+        .with(formatting_layer)
+}
+
+/// Register a subscriber as global default to process span data.
+pub fn init_subscriber(subscriber: impl Subscriber + Send + Sync) -> Result<(), ExitFailure> {
+    LogTracer::init()?;
+    Ok(set_global_default(subscriber)?)
+}
+
+#[actix_web::main]
+async fn main() -> Result<(), ExitFailure> {
+    let subscriber = get_subscriber("newsletter-rs".to_owned(),"info".to_owned());
+    init_subscriber(subscriber)?;
     let config_file: &str = "configuration.yaml";
     let configuration: ApplicationSettings =
         get_configuration(config_file).unwrap_or_else(|error| {
