134227f4620711687e52d748a78726001ef86b1a
Luis Jesus Diaz Manzo <luis@drconopoima.com>
2022-03-19 23:28:43 +0100

    Refactor subscription.rs into fns per tracing span

--- src/routes/subscription.rs
+++ src/routes/subscription.rs
@@ -1,8 +1,8 @@
 use actix_web::{web, HttpRequest, HttpResponse, Responder};
-use deadpool_postgres::Pool;
+use deadpool_postgres::{Object, Pool};
 use regex::Regex;
 use std::sync::Arc;
-use tracing::Instrument;
+use tokio_postgres::Statement;
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
@@ -11,17 +11,19 @@ pub struct SubscriptionFormData {
     name: String,
 }
 
+#[tracing::instrument(
+    name = "Processing incoming subscription.",
+    skip( form, request ),
+    fields(
+        request_id = %Uuid::new_v4(),
+        subscription_email = %form.email,
+        subscription_name = %form.name
+    )
+)]
 pub async fn subscription(
     request: HttpRequest,
     form: web::Form<SubscriptionFormData>,
 ) -> impl Responder {
-    let request_id = Uuid::new_v4();
-    let request_span = tracing::info_span!(
-        "Processing incoming subscription.",
-        %request_id, subscription_email=%form.email,
-        subscription_name=%form.name,
-    );
-    let _request_span_entered = request_span.enter();
     let optional_postgres_pool: Option<&Arc<Pool>> = match request.app_data::<Arc<Pool>>() {
         Some(postgres_pool) => Some(postgres_pool),
         None => {
@@ -34,18 +36,7 @@ pub async fn subscription(
             .body("DB pool error while processing subscription.");
     }
     let postgres_pool = optional_postgres_pool.unwrap();
-    let getclient_span = tracing::info_span!(
-        "Retrieving database client from pool.",
-        %request_id, subscription_email=%form.email,
-        subscription_name=%form.name,
-    );
-    let optional_postgres_client = match postgres_pool.get().instrument(getclient_span).await {
-        Ok(manager) => Some(manager),
-        Err(error) => {
-            tracing::error!("Could not retrieve postgres client from pool, {}.", error);
-            None
-        }
-    };
+    let optional_postgres_client = get_postgres_client(postgres_pool).await;
     if optional_postgres_client.is_none() {
         return HttpResponse::InternalServerError()
             .body("DB client error while processing subscription.");
@@ -59,19 +50,32 @@ pub async fn subscription(
             &form.email
         ));
     }
-    let preparestatement_span = tracing::info_span!(
-        "Preparing cached insert subscription query statement.",
-        %request_id, subscription_email=%form.email,
-        subscription_name=%form.name,
-    );
-    let statement = match postgres_client
+    run_insert_subscriber_query(postgres_client, form).await
+}
+
+#[tracing::instrument(name = "Retrieving database client from pool.", skip(postgres_pool))]
+pub async fn get_postgres_client(postgres_pool: &Arc<Pool>) -> Option<Object> {
+    match postgres_pool.get().await {
+        Ok(manager) => Some(manager),
+        Err(error) => {
+            tracing::error!("Could not retrieve postgres client from pool, {}.", error);
+            None
+        }
+    }
+}
+
+#[tracing::instrument(
+    name = "Preparing cached insert subscription query statement.",
+    skip(postgres_client)
+)]
+pub async fn prepare_cached_statement(postgres_client: &Object) -> Option<Statement> {
+    match postgres_client
         .prepare_cached(
             r#"
                 INSERT INTO newsletter.subscription (id, email, name)
                 VALUES ($1, $2, $3)
             "#,
         )
-        .instrument(preparestatement_span)
         .await
     {
         Ok(statement) => Some(statement),
@@ -82,22 +86,27 @@ pub async fn subscription(
             );
             None
         }
-    };
+    }
+}
+
+#[tracing::instrument(
+    "Running insert query to save subscription into database.",
+    skip(postgres_client, form)
+)]
+pub async fn run_insert_subscriber_query(
+    postgres_client: Object,
+    form: web::Form<SubscriptionFormData>,
+) -> HttpResponse {
+    let statement = prepare_cached_statement(&postgres_client).await;
     if statement.is_none() {
         return HttpResponse::InternalServerError().finish();
     }
     let generated_uuid: Uuid = Uuid::new_v4();
-    let query_span = tracing::info_span!(
-        "Running insert query to save subscription into database.",
-        %request_id, subscription_email=%form.email,
-        subscription_name=%form.name,
-    );
     match postgres_client
         .query(
             &statement.unwrap(),
             &[&generated_uuid, &form.email, &form.name],
         )
-        .instrument(query_span)
         .await
     {
         Ok(_) => HttpResponse::Ok().finish(),
