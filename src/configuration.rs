b2062bcc624158dc02cd64fb83ec69add9e36608
Luis Jesús Díaz Manzo <luis@drconopoima.com>
2024-08-10 16:31:28 +0200

    configuration.rs main.yaml add SMTP Settings

--- src/configuration.rs
+++ src/configuration.rs
@@ -23,6 +23,7 @@ pub struct ApplicationSettings {
     pub port: u16,
     #[serde(deserialize_with = "deserialize_option_number_from_string")]
     pub healthcachevalidityms: Option<u32>,
+    pub smtp: SmtpSettings,
 }
 
 #[derive(serde::Deserialize)]
@@ -114,6 +115,28 @@ impl DatabaseSettings {
             self.username, self.password, self.host, self.port
         )
     }
+}
+
+#[derive(serde::Deserialize)]
+pub struct SmtpSettings {
+    pub from: String,
+    pub name: String,
+    pub reply_to: String,
+    pub relay: SmtpRelaySettings,
+}
+
+#[derive(serde::Deserialize)]
+pub struct SmtpRelaySettings {
+    pub address: String,
+    #[serde(deserialize_with = "deserialize_option_number_from_string")]
+    pub port: Option<u16>,
+    pub credentials: SmtpRelayCredentialsSettings,
+}
+
+#[derive(serde::Deserialize)]
+pub struct SmtpRelayCredentialsSettings {
+    pub username: String,
+    pub password: CensoredString,
 }
 
 // Read top-level configuration file with extension YAML...
